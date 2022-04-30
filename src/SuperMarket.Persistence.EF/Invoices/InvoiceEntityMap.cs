c62089e08677bbb1e7125fe9d3eaf0bd5568f0d5
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 01:41:07 +0430

    added class invoiceEntityMap to persistence layer

---
+++ src/SuperMarket.Persistence.EF/Invoices/InvoiceEntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistence.EF.Invoices
+{
+    public class InvoiceEntityMap : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            builder.ToTable("Invoices");
+
+            builder.HasKey(_ => _.Id);
+            builder.Property(_ => _.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
