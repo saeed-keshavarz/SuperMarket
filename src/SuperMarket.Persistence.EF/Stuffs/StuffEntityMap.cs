1ba68a25eac391bc6921d11b6be61b1615a959b9
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 01:30:34 +0430

    added class stuffEntityMap to persistence layer

---
+++ src/SuperMarket.Persistence.EF/Stuffs/StuffEntityMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistence.EF.Stuffs
+{
+    public class StuffEntityMap : IEntityTypeConfiguration<Stuff>
+    {
+        public void Configure(EntityTypeBuilder<Stuff> builder)
+        {
+            builder.ToTable("Stuffs");
+
+            builder.HasKey(_ => _.Id);
+            builder.Property(_ => _.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.HasMany(_=>_.Invoces)
+                .WithOne(_=>_.Stuff)
+                .HasForeignKey(_=>_.StuffId)
+                .OnDelete(DeleteBehavior.ClientNoAction);
+
+            builder.HasMany(_ => _.Vouchers)
+                .WithOne(_ => _.Stuff)
+                .HasForeignKey(_ => _.StuffId)
+                .OnDelete(DeleteBehavior.ClientNoAction);
+
+        }
+    }
+}
