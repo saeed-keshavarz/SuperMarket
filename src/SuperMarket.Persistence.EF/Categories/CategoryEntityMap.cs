98301429b716c4026d19ec46534960ae4b07dcba
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 01:24:07 +0430

    added class CategoryEntityMap to persistence layer

---
+++ src/SuperMarket.Persistence.EF/Categories/CategoryEntityMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistence.EF.Categories
+{
+    public class CategoryEntityMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(_ => _.Id);
+            builder.Property(_ => _.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.HasMany(_=>_.Stuffs)
+                .WithOne(_=>_.Category)
+                .HasForeignKey(_=>_.CategoryId)
+                .OnDelete(DeleteBehavior.ClientNoAction);
+        }
+    }
+}
