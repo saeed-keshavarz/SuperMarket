6fbf93292556f738b2b815337f77fa470b5376d6
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 01:44:53 +0430

    added class voucherEntityMap to persistence layer

---
+++ src/SuperMarket.Persistence.EF/Vouchers/VoucherEntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistence.EF.Vouchers
+{
+    public class VoucherEntityMap : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.ToTable("Vouchers");
+
+            builder.HasKey(_=>_.Id);
+            builder.Property(_ => _.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
