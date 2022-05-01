87e52768fa2c224b209e5249fab6b9861a9393ae
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 20:04:06 +0430

    added class BDDHelper, configurationFixture and efdatacontextDatabaseFixture to specs layer

---
+++ src/SuperMarket.Specs/Infrastructure/EFDataContextDatabaseFixture.cs
@@ -0,0 +1,21 @@
+﻿
+using Xunit;
+
+namespace SuperMarket.Specs.Infrastructure
+{
+    [Collection(nameof(ConfigurationFixture))]
+    public class EFDataContextDatabaseFixture : DatabaseFixture
+    {
+        readonly ConfigurationFixture _configuration;
+
+        public EFDataContextDatabaseFixture(ConfigurationFixture configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public EFDataContext CreateDataContext()
+        {
+            return new EFDataContext(_configuration.Value.DbConnectionString);
+        }
+    }
+}
