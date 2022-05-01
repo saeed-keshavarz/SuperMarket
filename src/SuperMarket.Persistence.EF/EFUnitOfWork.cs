412916446cd1e46a50880adaf0d2b28167060f2d
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 20:35:09 +0430

    added class EFUintOfWork to persistence layer

---
+++ src/SuperMarket.Persistence.EF/EFUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using SuperMarket.Infrastructure.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistence.EF
+{
+    public class EFUnitOfWork : UnitOfWork
+    {
+        private readonly EFDataContext _dataContext;
+        public EFUnitOfWork(EFDataContext dataConext)
+        {
+            _dataContext = dataConext;
+        }
+
+        public void Commit()
+        {
+            _dataContext.SaveChanges();
+        }
+    }
+}
