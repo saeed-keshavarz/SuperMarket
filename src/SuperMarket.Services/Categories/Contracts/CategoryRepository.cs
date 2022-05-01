7bc49e55ba567b4d7d2c006e1b85bf025fc6de2c
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-01 22:27:08 +0430

    added interface categoryRepository to service layer

---
+++ src/SuperMarket.Services/Categories/Contracts/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using SuperMarket.Infrastructure.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Services.Categories.Contracts
+{
+    public interface CategoryRepository :Repository
+    {
+    }
+}
