4e755f5e361172cf5b02babbd3cc2eb9074adde7
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-08 01:45:39 +0430

    added class UpdateInvoice to specs layer / added method update to invoiceService in service layer

--- src/SuperMarket.Services/Invoices/Contracts/InvoiceService.cs
+++ src/SuperMarket.Services/Invoices/Contracts/InvoiceService.cs
@@ -10,5 +10,6 @@ namespace SuperMarket.Services.Invoices.Contracts
     public interface InvoiceService : Service
     {
         void Add(AddInvoiceDto dto, int stuffId);
+        void Update(int id1, UpdateInvoiceDto dto, int id2, int quantity);
     }
 }
