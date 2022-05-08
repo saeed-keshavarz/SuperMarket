fa51fe12dfadb9eceea41063fb1cc29be5bf7bad
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-08 13:16:02 +0430

    added method Add_adds_category_properly() to class CategoryServiceTest in service.Test.Unit layer

---
+++ src/SuperMarket.Services.Test.Unit/Categories/CategoryServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using SuperMarket.Infrastructure.Application;
+using SuperMarket.Infrastructure.Test;
+using SuperMarket.Persistence.EF;
+using SuperMarket.Persistence.EF.Categories;
+using SuperMarket.Services.Categories;
+using SuperMarket.Services.Categories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SuperMarket.Services.Test.Unit.Categories
+{
+    public class CategoryServiceTests
+    {
+        private readonly EFDataContext _dataContext;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly CategoryService _sut;
+        private readonly CategoryRepository _repository;
+
+        public CategoryServiceTests()
+        {
+            _dataContext =
+               new EFInMemoryDatabase()
+               .CreateDataContext<EFDataContext>();
+            _unitOfWork = new EFUnitOfWork(_dataContext);
+            _repository = new EFCategoryRepository(_dataContext);
+            _sut = new CategoryAppService(_repository, _unitOfWork);
+        }
+
+        [Fact]
+        public void Add_adds_category_properly()
+        {
+            AddCategoryDto dto = GenerateAddCategoryDto();
+
+            _sut.Add(dto);
+
+            _dataContext.Categories.Should()
+                .Contain(_ => _.Title == dto.Title);
+        }
+
+        private static AddCategoryDto GenerateAddCategoryDto()
+        {
+            return new AddCategoryDto
+            {
+                Title = "dummy"
+            };
+        }
+    }
+}
