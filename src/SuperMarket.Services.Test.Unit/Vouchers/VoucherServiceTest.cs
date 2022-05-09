698854618957911ee226815b97c31cb654fe5788
Saeed Keshavarz <saeed.keshavarzi@outlook.com>
2022-05-09 10:29:07 +0430

    added method Add_add_voucher_properly() to class VoucherServiceTest

--- src/SuperMarket.Services.Test.Unit/Vouchers/VoucherServiceTest.cs
+++ src/SuperMarket.Services.Test.Unit/Vouchers/VoucherServiceTest.cs
@@ -1,4 +1,7 @@
-﻿using SuperMarket.Infrastructure.Application;
+﻿using FluentAssertions;
+using Supermarket.Test.Tools.Categories;
+using Supermarket.Test.Tools.Stuffs;
+using SuperMarket.Infrastructure.Application;
 using SuperMarket.Infrastructure.Test;
 using SuperMarket.Persistence.EF;
 using SuperMarket.Persistence.EF.Vouchers;
@@ -9,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SuperMarket.Services.Test.Unit.Vouchers
 {
@@ -28,6 +32,37 @@ namespace SuperMarket.Services.Test.Unit.Vouchers
             _sut = new VoucherAppService(_repository, _unitOfWork);
         }
 
+        [Fact]
+        public void Add_add_voucher_properly()
+        {
+            var category = CategoryFactory.CreateCategory("لبنیات");
+            _dataContext.Manipulate(_ => _.Categories.Add(category));
 
+            var stuff = StuffFactory.CreateStuff(category, "پنیر");
+            _dataContext.Manipulate(_ => _.Stuffs.Add(stuff));
+
+            AddVoucherDto dto = GenerateAddVoucherDto(stuff, stuff.Title);
+
+            _sut.Add(dto,stuff.Id);
+
+            _dataContext.Vouchers.Should()
+                .Contain(_ =>
+                _.Title == dto.Title &&
+                _.Date==dto.Date &&
+                _.Quantity==dto.Quantity &&
+                _.Price==dto.Price);
+        }
+
+        private AddVoucherDto GenerateAddVoucherDto(Entities.Stuff stuff, string title)
+        {
+            return new AddVoucherDto
+            {
+                Title = "سند: " + stuff.Title,
+                Date = new DateTime(1401, 02, 18),
+                Quantity = 10,
+                Price = 1000,
+                StuffId = stuff.Id,
+            };
+        }
     }
 }
